760ab18ac12a94e709b3732836f410c16b539ba4
Fazlul-Hoque1 <165183616+Fazlul-Hoque1@users.noreply.github.com>
2025-11-23 16:28:50 +0100

    adding the event table column
    Added use statement and created event table with fields.

--- Database/projectDB.sql
+++ Database/projectDB.sql
@@ -2,3 +2,14 @@
 create DATABASE userdb;
 create DATABASE eventdb;
 
+-- using use eventdb to access the main table
+USE eventdb;
+
+-- adding necessary fields and attributes
+CREATE TABLE event (
+	event_id INT NOT NULL AUTO_INCREMENT,
+    title varchar(10),
+    date_e  varchar(10),
+    location varchar(10),
+    description_n varchar(50)
+    )
