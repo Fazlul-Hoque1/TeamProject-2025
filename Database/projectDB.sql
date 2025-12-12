9bd2cd8d3788ef80a2cce19518cb1fc511f64f0a
Fazlul-Hoque1 <165183616+Fazlul-Hoque1@users.noreply.github.com>
2025-12-12 15:09:56 +0100

    Update database creation script for app_user

--- Database/projectDB.sql
+++ Database/projectDB.sql
@@ -1,10 +1,9 @@
 -- creating the databases
-CREATE DATABASE userdb;
 CREATE DATABASE eventdb;
-CREATE DATABASE testeventdb;
+CREATE DATABASE app_user;
 
 -- main event table
-USE eventdb;
+-- USE eventdb;
 
 -- adding necessary fields and attributes
 CREATE TABLE event ( 
@@ -16,17 +15,15 @@ CREATE TABLE event (
     PRIMARY KEY (event_id)
 );
 
--- test event table
-USE testeventdb;
+-- user table
+USE app_user;
 
-CREATE TABLE event (
-    event_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(55),
-    date_e VARCHAR(55),
-    location VARCHAR(55),
-    description_n VARCHAR(150),
-    PRIMARY KEY (event_id)
+-- adding the necessary fields and attributes for the app_user db table
+CREATE TABLE users (
+    user_id INT,
+    username VARCHAR(50),
+    password VARCHAR(255),
+    email VARCHAR(100),
+    role VARCHAR(50),
+    PRIMARY KEY (user_id),
 );
-
--- check contents of test table (should be empty)
-SELECT * FROM event;
