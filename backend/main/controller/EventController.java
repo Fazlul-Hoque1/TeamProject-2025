c8bf812baddb04b51b764931a6b5be0053c88918
Zain Afzaal <zainafzaal7890@gmail.com>
2025-12-24 15:54:44 +0100

    Update EventController.java

--- backend/main/controller/EventController.java
+++ backend/main/controller/EventController.java
@@ -1,11 +1,16 @@
-
 package com.eventorganizer.event_org.controller;
 
+import com.eventorganizer.event_org.model.App_User;
 import com.eventorganizer.event_org.model.Event;
 import com.eventorganizer.event_org.repository.EventRepository;
+import com.eventorganizer.event_org.repository.UserRepository;
+import com.eventorganizer.event_org.service.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,30 +19,70 @@ import java.util.List;
 public class EventController {
 
     @Autowired
-    private EventRepository repo;
+    private EventRepository eventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
 
 
     @PostMapping("/add")
     public Event addEvent(@RequestBody Event event) {
-        return repo.save(event);
+
+        String username  = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        App_User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        event.setCreatedBy(user.getUsername());
+
+        return eventRepository.save(event);
     }
+
+
+
+    @PostMapping("/join/{id}")
+    public Event joinEvent(@PathVariable Long id) {
+
+        String username  = SecurityContextHolder.getContext().getAuthentication().getName();
+        App_User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Event event = eventRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Event not found"));
+
+        if(event.getJoinedBy()== null){
+            event.setJoinedBy(new ArrayList<>());
+        }
+
+        if (!event.getJoinedBy().contains(user.getUsername())){
+            event.getJoinedBy().add(user.getUsername());
+            eventRepository.save(event);
+
+        }
+
+        return eventRepository.save(event);
+    }
+
+
 
 
     @GetMapping("/all")
     public List<Event> getAllEvents() {
-        return repo.findAll();
+        return eventRepository.findAll();
     }
 
 
     @GetMapping("/{id}")
     public Event getEvent(@PathVariable Long id) {
-        return repo.findById(id)
+        return eventRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Event with ID " + id + " not found"));
     }
 
+    // UPDATE (PUT)
     @PutMapping("/update/{id}")
     public Event updateEvent(@PathVariable Long id, @RequestBody Event newEvent) {
-        Event existing = repo.findById(id)
+        Event existing = eventRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Event with ID " + id + " not found"));
 
         existing.setTitle(newEvent.getTitle());
@@ -45,21 +90,61 @@ public class EventController {
         existing.setDate(newEvent.getDate());
         existing.setDescription(newEvent.getDescription());
 
-        return repo.save(existing);
+        return eventRepository.save(existing);
     }
+
+    // PARTIAL UPDATE (PATCH)-> IT ONLY WORKS IN LOCAL HOST NOT ON "CLOUD PLATFORM"
+    @PatchMapping("/update-partial/{id}")
+    public Event updatePartial(@PathVariable Long id, @RequestBody Event eventData) {
+        Event existing = eventRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        if (eventData.getTitle() != null)
+            existing.setTitle(eventData.getTitle());
+
+        if (eventData.getDate() != null)
+            existing.setDate(eventData.getDate());
+
+        if (eventData.getLocation() != null)
+            existing.setLocation(eventData.getLocation());
+
+        if (eventData.getDescription() != null)
+            existing.setDescription(eventData.getDescription());
+
+        return eventRepository.save(existing);
+    }
+
+    // DELETE
+//    @DeleteMapping("/delete/{id}")
+//    public String deleteEvent(@PathVariable Long id) {
+//        if (!eventRepository.existsById(id)) {
+//            throw new IllegalArgumentException("Event with ID " + id + " not found");
+//        }
+//
+//        eventRepository.deleteById(id);
+//        return "Event deleted successfully.";
+//    }
 
     @DeleteMapping("/delete/{id}")
     public String deleteEvent(@PathVariable Long id) {
-        if (!repo.existsById(id)) {
-            throw new IllegalArgumentException("Event with ID " + id + " not found");
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        String role = SecurityContextHolder.getContext().getAuthentication().getAuthorities().iterator().next().getAuthority();
+
+        Event event = eventRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("event not found"));
+
+        if(role.equals("ROLE_ADMIN")){
+            eventRepository.delete(event);
+            return "Event deleted Successfully by Admin";
         }
 
-        repo.deleteById(id);
-        return "Event deleted successfully.";
+        if(!event.getCreatedBy().equals(username)){
+            throw new IllegalArgumentException("You are not allowed to delete this event");
+        }
+
+
+        eventRepository.delete(event);
+        return "Event deleted.";
     }
-
-    
-
-  
 }
-
